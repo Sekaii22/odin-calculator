131eccc9bd1a6c4903b73402e383192f2b21fc58
Sekaii22 <shawn.ng22@gmail.com>
2025-04-05 07:41:55 +0800

    Fix: multiple issues with digits appending to display when not supposed to
    Pressing a new digit when a result is displayed no
    longer append it to existing result.
    Also no longer append digits to error message.

--- calculator.js
+++ calculator.js
@@ -6,23 +6,46 @@ let operators = {
 }
 
 function operate(a, op, b) {
-    return operators[op](a, b);
+    if (op === "/" && b === 0) return "ERROR: divide by 0";
+    let result = operators[op](a, b);
+    console.log(result);
+
+    return +result.toFixed(8);       // at most 8 decimal place;
+}
+
+function clear() {
+    display.textContent = "";
+    fullExpression.textContent = "";
+    num1 = null;
+    num2 = null;
+    op = null;
 }
 
 let display = document.querySelector("#display");
 let fullExpression = document.querySelector('#full-expression');
-let num1, num2, op;
+let num1, num2, op = null;
+let lastProcessed = "";
 
 // number btns
 document.querySelectorAll(".number")
         .forEach((numBtn) => {
             numBtn.addEventListener("click", () => {
+                if (lastProcessed === "=") clear();
+
+                // prepare display for 2nd number
+                if (op !== null && num2 === null) display.textContent = "";
+
                 // append to display
-                display.textContent = display.textContent + numBtn.textContent;
+                display.textContent += numBtn.textContent;
+                lastProcessed = numBtn.textContent
 
                 // store current value depending if op is set
-                if (!op) num1 = +display.textContent;
-                else num2 = +display.textContent;
+                if (op === null) {
+                    num1 = +display.textContent;
+                }
+                else {
+                    num2 = +display.textContent;
+                }
             });
         });
 
@@ -30,42 +53,62 @@ document.querySelectorAll(".number")
 document.querySelectorAll(".op")
         .forEach((opBtn) => {
             opBtn.addEventListener("click", () => {
-                if (!op) {
-                    op = opBtn.textContent;
+                if (num1 !== null) {
+                    if (op === null || (op !== null && num2 === null)) {
+                        // if no op or if consecutive op btn is pressed, set to last op entered
+                        op = opBtn.textContent;
+                        fullExpression.textContent = num1 + " " + op;
+                    }
+                    else {
+                        // if num1, op, num2 already have values when op btn is pressed,
+                        // evaluate the result first, before assigning the next op
+                        let result = operate(num1, op, num2);
+                        display.textContent = result;
+                        op = opBtn.textContent;  
+                        fullExpression.textContent += " " + num2 + " " + op;
+                        
+                        // check for error
+                        if (typeof result !== "number") {
+                            num1 = null;                     // only have to set num1 to null to stop any operators or eval
+                            lastProcessed = "=";
+                            return;
+                        };
+                        
+                        num1 = result;
+                        num2 = null;
+                    }
+    
+                    lastProcessed = opBtn.textContent;
                 }
-                else {
-                    // evaluate the result first
-                    let result = operate(num1, op, num2);
-                    num1 = result;
-                    num2 = null;
-                    
-                    op = opBtn.textContent;          
-                }
-
-                display.textContent = "";
-                fullExpression.textContent = num1 + " " + op;
             });
         });
 
 // eval btn
 document.querySelector(".eval").addEventListener("click", () => {
     // dont allow eval before all numbers and operator has been inputted
-    if (num1 && op && num2) {
+    if (num1 !== null && op !== null && num2 !== null) {
         let result = operate(num1, op, num2);
         display.textContent = result;
-        fullExpression.textContent = fullExpression.textContent + " " + num2 + " =";
+
+        fullExpression.textContent += " " + num2 + " =";
+        lastProcessed = "=";
     
+        // check for error
+        if (typeof result !== "number") {
+            num1 = null;                                    // prevents further calculations
+            return;
+        }
+
         num1 = result;
-        num2 = null;
         op = null;
+        num2 = null;
     }
 })
 
 // clear btn
-document.querySelector(".clear").addEventListener("click", () => {
-    display.textContent = "";
-    fullExpression.textContent = "";
-    num1 = null;
-    num2 = null;
-    op = null;
-})
+document.querySelector(".clear").addEventListener("click", clear);
+
+// TODO: set default num1 value as 0
+
+// TODO: Add event for divide by zero error, 
+// and for handling, disable all buttons except numbers, clear, and back.

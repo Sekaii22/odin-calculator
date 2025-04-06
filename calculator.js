7e4243e76fa1a5027f914e1448f6336878d8db2c
Sekaii22 <shawn.ng22@gmail.com>
2025-04-07 04:50:49 +0800

    Fix: long string of numbers causing UI to expand. Added on/off functionality
    Enabled horizontal scroll on display and fullExpression
    div.

--- calculator.js
+++ calculator.js
@@ -7,10 +7,37 @@ let operators = {
 
 function operate(a, op, b) {
     if (op === "/" && b === 0) return "ERROR: divide by 0";
+    if (!isFinite(a) || !isFinite(b)) return "ERROR: infinity";
+    
     let result = operators[op](a, b);
+    
+    if (!isFinite(result)) return "ERROR: infinity";
+    
+    result = result.toFixed(8);       // at most 8 decimal place;
     console.log(result);
 
-    return +result.toFixed(8);       // at most 8 decimal place;
+    // if (result.length > maxCharLength) {
+    //     if (result.includes(".")) {
+    //         // remove dp number from back until within max char length
+    //         let dpIndex =  result.indexOf(".");
+    //         for (let i = result.length - 1; i >= dpIndex; i--) {
+    //             result = result.slice(0, -1);
+
+    //             if (result.length <= maxCharLength) break;
+    //         }
+
+    //         // if after removal still > max char length
+    //         if (result.length > maxCharLength) return "ERROR: too large";
+
+    //         // remove it if last char is "."
+    //         if (result.at(-1) === ".") result = result.slice(0, -1);
+    //     }
+    //     else {
+    //         return "ERROR: too large";
+    //     }
+    // }
+
+    return +result;
 }
 
 function clear() {
@@ -33,6 +60,9 @@ function numberPress(keyedNum) {
     if (op !== null && num2 === null) {
         display.textContent = "0";
     }
+
+    // check if number length is at max alr
+    //if (display.textContent.length >= maxCharLength) return;
 
     // append to display
     display.textContent = (display.textContent === "0") ? keyedNum : display.textContent + keyedNum;
@@ -112,6 +142,8 @@ function dpPress() {
     }
 
     if (!display.textContent.includes(".")) {
+        //if (display.textContent.length >= maxCharLength) return;
+
         display.textContent += ".";
         lastProcessed = ".";
     }
@@ -137,9 +169,11 @@ function backspacePress() {
         // store current value depending if op is set
         if (op === null) {
             num1 = parseFloat(display.textContent);
+            console.log(num1);
         }
         else {
             num2 = parseFloat(display.textContent);
+            console.log(num1);
         }
     }
     
@@ -154,11 +188,16 @@ function negatePress() {
         
         // store current value depending if op is set
         if (op === null) {
+            // only allow positive to negative toggle at max length
+            //if ((display.textContent.length >= maxCharLength) && (num1 > 0)) return;
+            
             if (lastProcessed === "=") fullExpression.textContent = "";
             num1 *= -1;
             display.textContent = num1;
         }
         else {
+            //if ((display.textContent.length >= maxCharLength) && (num2 > 0)) return;
+
             num2 *= -1;
             display.textContent = num2;
         }
@@ -177,9 +216,13 @@ let fullExpression = document.querySelector('#full-expression');
 let num1 = 0;
 let num2 = null;
 let op = null;
+
 let lastProcessed = "";
 let error = false;
+let isOn = true;
+//let maxCharLength = 14;
 
+let debugTexts = document.querySelector(".debug-texts");
 let num1Display = document.querySelector("#num1-text");
 let opDisplay = document.querySelector("#op-text");
 let num2Display = document.querySelector("#num2-text");
@@ -191,7 +234,7 @@ num2Display.textContent = String(num2);
 document.querySelectorAll(".number")
         .forEach((numBtn) => {
             numBtn.addEventListener("click", () => {
-                numberPress(numBtn.textContent);
+                if (isOn) numberPress(numBtn.textContent);
             });
         });
 
@@ -199,63 +242,93 @@ document.querySelectorAll(".number")
 document.querySelectorAll(".op")
         .forEach((opBtn) => {
             opBtn.addEventListener("click", () => {
-                operatorPress(opBtn.textContent);
+                if (isOn) operatorPress(opBtn.textContent);
             });
         });
 
 // eval btn
-document.querySelector(".eval").addEventListener("click", evalPress);
+document.querySelector(".eval").addEventListener("click", () => {
+    if (isOn) evalPress();
+});
 
 // clear btn
-document.querySelector(".clear").addEventListener("click", clear);
+document.querySelector(".clear").addEventListener("click", () => {
+    if (isOn) clear();
+});
 
 // decimal point btn
-document.querySelector(".dp").addEventListener("click", dpPress);
+document.querySelector(".dp").addEventListener("click", () => {
+    if (isOn) dpPress();
+});
 
 // back btn
-document.querySelector(".back").addEventListener("click", backspacePress);
+document.querySelector(".back").addEventListener("click", () => {
+    if (isOn) backspacePress();
+});
 
 // negate sign btn
-document.querySelector(".negate").addEventListener("click", negatePress);
+document.querySelector(".negate").addEventListener("click", () => {
+    if (isOn) negatePress();
+});
 
 // keyboard support
 document.addEventListener("keydown", (e) => {
-    let keyPressed = e.key;
-    console.log(keyPressed);
+    if (isOn) {
+        let keyPressed = e.key;
+        console.log(keyPressed);
+    
+        if (isNaN(keyPressed) === false) {
+            // number key pressed
+            numberPress(keyPressed);
+        }
+        else if (Object.keys(operators).includes(keyPressed)) {
+            // operator key pressed
+            operatorPress(keyPressed);
+        }
+        else if (keyPressed === "=") {
+            // eval
+            evalPress();
+        }
+        else if (keyPressed === "Escape") {
+            // clear
+            clear();
+        }
+        else if (keyPressed === ".") {
+            //decimal point pressed
+            dpPress();
+        }
+        else if (keyPressed === "Backspace") {
+            backspacePress();
+        }
+    }
+});
 
-    if (isNaN(keyPressed) === false) {
-        // number key pressed
-        numberPress(keyPressed);
+// on off btn
+document.querySelector(".on").addEventListener("click", () => {
+    clear()
+    isOn = !isOn;
+
+    if (isOn === true) {
+        console.log("ON calculator");
+        debugTexts.style.visibility = "visible";
     }
-    else if (Object.keys(operators).includes(keyPressed)) {
-        // operator key pressed
-        operatorPress(keyPressed);
-    }
-    else if (keyPressed === "=") {
-        // eval
-        evalPress();
-    }
-    else if (keyPressed === "Escape") {
-        // clear
-        clear();
-    }
-    else if (keyPressed === ".") {
-        //decimal point pressed
-        dpPress();
-    }
-    else if (keyPressed === "Backspace") {
-        backspacePress();
+    else {
+        console.log("OFF calculator");
+        debugTexts.style.visibility = "hidden";
+        display.textContent = "";
     }
 });
 
 // update debug texts
-document.querySelectorAll("button")
+document.querySelectorAll("button:not(.on)")
         .forEach((btn) => {
             btn.addEventListener("click", updateDebugDisplay);
         });
 
-
 // BUG: chaining lots of operator or when digits entered is very long, display will stretch
+// max is 14 char before display warps
+// restrict value keyed to 14 char (DONE)
+// when result after removing all decimal numbers are still more than 14 digits, show error: number too large (DONE)
 
 // TODO: Add event for divide by zero error, 
 // and for handling, disable all buttons except numbers, clear, and back.
